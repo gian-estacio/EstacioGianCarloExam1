e83acd1b49e09acb9d1d6bcbdb8798f7388f757c
gian-estacio <2015085933@ust-ics.mygbiz.com>
2018-10-06 09:55:28 +0800

    Initial commit

---
+++ app/src/main/java/giancarlo/estacio/com/estaciogiancarloexam1/MainActivity.java
@@ -0,0 +1,17 @@
+package giancarlo.estacio.com.estaciogiancarloexam1;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
